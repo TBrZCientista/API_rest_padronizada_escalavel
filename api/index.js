18f7aca252acc560cc0dd5421fd0019502e1fe60
Felipe <felipefreitasdaschagas@hotmail.com>
2022-04-04 14:02:53 -0300

    persintindo dados na tebela

--- api/index.js
+++ api/index.js
@@ -6,6 +6,6 @@ const config = require('../config/defaut.json')
 app.use(bodyParser.json)
 
 const roteador = require('./rotas/fornecedores')
-app.use ('/api/rotas/fornecedores', roteador) 
+app.use ('./api/rotas/fornecedores', roteador) 
 
 app.listen(config.api.porta, () => console.log('A API está funcionando!!!'))
--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,10 +1,20 @@
 const roteador = require('express').Router()
 const tabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
 
-roteador.use('./api/rotas/fornecedores', async(requisicao, resposta) => {
+roteador.get('./api/rotas/fornecedores', async(requisicao, resposta) => {
     const resultados = await tabelaFornecedor.listar()
     resposta.send(JSON.stringify(resultados))
 })
+
+roteador.post('./api/rotas/fornecedores', async (requisicao,resposta) => {
+    const dadosRecebidos = requisicao.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    resposta.send (
+        JSON.stringify(fornecedor)
+    )
+} )
 
 module.exports = roteador
