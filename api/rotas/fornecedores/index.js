fa8a88b75bd1998c439c31936c5529687a133d76
Felipe <felipefreitasdaschagas@hotmail.com>
2022-04-05 14:09:28 -0300

    melhorando os status de resposta de erros

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -19,6 +19,7 @@ roteador.post('./api/rotas/fornecedores', async (requisicao,resposta) => {
         JSON.stringify(fornecedor)
     )
     } catch (erro){
+        resposta.status(400)
         resposta.send(
             JSON.stringify({
                 mensagem: erro.message
@@ -38,6 +39,7 @@ roteador.get('/:idFornecedor', async (requisicao,resposta) => {
         )
     }
     catch(erro){
+        resposta.status(404)
         resposta.send(
             JSON.stringify({
                mensagem : erro.message 
@@ -57,6 +59,7 @@ roteador.put('/:idFornecedor', async (requisicao,resposta) => {
         resposta.end()
     }
     catch(erro) {
+        resposta.status(400)
         resposta.send(
             JSON.stringify({
                 mensagem: erro.message
@@ -74,6 +77,7 @@ roteador.delete('/:idFornecedor', async (requisicao,resposta) => {
         resposta.end()
         resposta.status(204)
     } catch {
+        resposta.status(404)
         resposta.send(
             JSON.stringify({
                 mensagem: erro.message
